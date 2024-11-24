1e11da5aba088ba17917e54b3c5bae45cb1a708c
azikenp <azikenp26@gmail.com>
2024-11-24 14:51:02 +0100

    refactoring

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -3,11 +3,11 @@ import { createContext, useRef, useState } from "react";
 export const AppContext = createContext({});
 
 const AppProvider = ({ children }) => {
-    const refs = {
-        section1: useRef(null),
-        section2: useRef(null),
-        section3: useRef(null)
-      }; 
+  const refs = {
+    section1: useRef(null),
+    section2: useRef(null),
+    section3: useRef(null),
+  };
   const [hasParentWithId, setHasParentWithId] = useState(false);
 
   const checkParentId = (element, id) => {
@@ -19,7 +19,6 @@ const AppProvider = ({ children }) => {
     }
     return false;
   };
-
 
   return (
     <AppContext.Provider
